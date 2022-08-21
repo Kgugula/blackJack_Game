568d76f3cbb47d07dda7f805d7f0e65494fd4719
Keiran <keiran.gugula@gmail.com>
2022-08-21 11:47:45 -0400

    finished code

--- pyScripts/game.py
+++ pyScripts/game.py
@@ -1,23 +1,44 @@
 from deck import Deck
 from hand import Hand
-
+from player import Player
 
 class Game:
     MINIMUM_BET = 1
+    ROUND = 1
 
     def __init__(self, player, dealer):
         self.player = player
         self.dealer = dealer
         self.bet = None
         self.deck = Deck()
+        self.exit_program = False
+
+    @staticmethod
+    def end_game(player, dealer):
+        try:
+            new_game = Game(player, dealer)
+            return new_game.start_game()
+            
+        except:
+            print("The program has ended.")
 
     def start_game(self):
 
         # While loop to confirm if user (player) would like to "join" table
         while True:
             userInput = input(f"You are staring with ${self.player.balance}. Would you like to play a hand? ")
+
+            # If not the first round; use class method clear_hand() on hand objects
+            if Game.ROUND != 1:
+                self.player.hand.clear_hand()
+                self.dealer.hand.clear_hand()
+                
             if userInput.lower() == "yes":
                               break
+            elif userInput.lower() == "no":
+                self.exit_program = True
+                print("Exit Program.")
+                Game.end_game()
 
         # While loop to prompt user to enter bet amount. Subject to contraints
         while True:
@@ -64,10 +85,20 @@ class Game:
 
         cards_to_deal -= 1
 
+        # Variable for edge case; player has a natural blackjack
+
+        player_natural_blackjack = False
+
         # While loop for player turn
 
         while True:
 
+            # Edge case for if its a natural blackjack
+            # Want to exit turn - no prompt for hit or stay
+            if self.player.hand.get_value() == 21 and len(self.player.hand) == 2:
+                player_natural_blackjack = True
+                break
+            
             playerTurn = input("Would you like to hit or stay? ")
             if playerTurn.lower() == "hit":
 
@@ -81,7 +112,8 @@ class Game:
                 if self.player.hand.get_value() > 21:
                     print(f"Your hand value is over 21 and you lose ${self.bet} :(")
                     self.player.balance -= self.bet
-                    break
+                    Game.ROUND += 1
+                    return Game.end_game(self.player, self.dealer)
 
             elif playerTurn.lower() == "stay":
                 break
@@ -96,32 +128,58 @@ class Game:
         self.dealer.hand.cards[1] = dealer_placeholder
         print(f"The dealer has: {self.dealer.hand}")
 
+        # Edge case when player has blackjack
+         
+        while player_natural_blackjack:
+            if self.dealer.hand.get_value() == 21:
+                print("Two natural blackjacks!! You win $50,000")
+                self.player.balance += 50000
+                Game.ROUND += 1
+                return Game.end_game(self.player, self.dealer)
+            else:
+                print(f"Blackjack! You win ${round((1.5 * self.bet))}")
+                self.player.balance += round((1.5 * self.bet))
+                Game.ROUND += 1
+                return Game.end_game(self.player, self.dealer)
+
+        
         # Check to see if dealer 'needs' to hit
 
         while self.dealer.hit():
 
             self.dealer.hand.add_to_hand(self.deck.deal(cards_to_deal))
-            print(f"The dealer hits and is dealt: {self.dealer.hand[-1]}")
+            print(f"The dealer hits and is dealt: {self.dealer.hand.cards[-1]}")
             print(f"The dealer has: {self.dealer.hand}")
             # If dealer busts off this hit, game is over. Double the bet amount and assign to player balance
             if self.dealer.hand.get_value() > 21:
                 print(f"The dealer busts, you win ${self.bet} :)")
                 self.player.balance += (2 * self.bet)
+                Game.ROUND += 1
+                return Game.end_game(self.player, self.dealer)
 
+
+        
+        
         # If the value of dealer cards >= 17; they must stay
-
+        
         print("The dealer stays.")
 
         # If neither player or dealer busts during their turn; need to compare value of the hands
 
         if self.player.hand.get_value() > self.dealer.hand.get_value():
-            print("You win ${self.bet}")
+            print(f"You win ${self.bet}")
             self.player.balance += (2 * self.bet)
+        elif self.player.hand.get_value() == self.dealer.hand.get_value():
+            print(f"Tie game! Bet is returned.")
+            Game.ROUND += 1
+            return Game.end_game(self.player, self.dealer)
+        else:
+            print(f"The dealer wins, you lose ${self.bet} :(")
+            self.player.balance -= self.bet
 
-        print(f"The dealer wins, you lose ${self.bet} :(")
-        self.player.balance -= self.bet
+        Game.ROUND += 1
 
-        
+        return Game.end_game(self.player, self.dealer)
 
             
 
--- pyScripts/hand.py
+++ pyScripts/hand.py
@@ -29,17 +29,43 @@ class Hand:
         for card in self.cards:
             values_list.append(Hand.CARD_VALUES[card[0]])
 
+        # Ignore value add of aces until the optimal value determined; below
         for value in values_list:
                 if value != "A":
                     values += value
 
         # While there is an ace in the hand - check values parameter to see what value of ace is optimal
-        while values_list.count("A") >= 1:
+        # Edge case if more than one ace in hand
 
-            if values <= 10:
+        '''
+        Enter while loop when hand has MULTIPLE aces.
+        We know the at most only one ace can hold the value of '11,'
+        i.e. if two aces both held '11' hand.get_value() > 21.
+        If current value of 'values' variable is less than '9'
+        then we have room for one ace to hold and '11' and ALL subsequent aces
+        must hold a '1.' Note that after adding += 11 when the first ace is removed,
+        values variable will ALWAYS be greater than '9' and we will not enter first
+        'if' statement below. Need to keep second 'if' statement in 'while' loop below
+        in case there are 3 or 4 aces in hand. 
+
+        '''
+
+        while values_list.count("A") > 1:
+            if values < 9:
                 values_list.remove("A")
                 values += 11
+            else:
+                values_list.remove("A")
+                values += 1
+            
+        # While there is only one ace in hand; different conditions
+        # Will not enter above 'while' loop
 
+        if values_list.count("A") == 1:
+        
+            if values <=10:
+                values_list.remove("A")
+                values += 11
             else:
                 values_list.remove("A")
                 values += 1
@@ -52,10 +78,35 @@ class Hand:
         for card in cards_dealt:
             self.cards.append(card)
 
+    # Method to clear hand; for the start of new rounds
+    def clear_hand(self):
 
+        self.cards *= 0
+           
     def __str__(self):
 
         str_hand = ', '.join([str(x)for x in self.cards])
         return str_hand
 
         # player script has identical function. Necessary?
+
+    def __len__(self):
+        count = 0
+        for card in self.cards:
+            count += 1
+        return count
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
